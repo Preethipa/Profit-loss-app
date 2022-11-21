ae9bb543248e8b7bbfed810fedaad0f1377c774d
PREETHI <preethirajavel06@gmail.com>
2022-11-21 16:14:55 +0530

    scripting is done

---
+++ app.js
@@ -0,0 +1,43 @@
+const initialPrice = document.querySelector("#initial-price");
+const stackQty = document.querySelector("#stack-qty");
+const currentPrice = document.querySelector("#current-price");
+
+const tellmeBtn = document.querySelector("#tellme-btn");
+const outputShown = document.querySelector("#output-shown");
+
+tellmeBtn.addEventListener("click", submitHandler);
+
+function submitHandler(){
+    var ip = initialPrice.value;
+    var qty = stackQty.value;
+    var cp = currentPrice.value;
+
+    calculateProfitAndLoss(ip, qty, cp);
+}
+
+
+function calculateProfitAndLoss(initialprice, stackqty, currentprice){
+    if(initialprice > currentprice){
+        var loss = (initialprice - currentprice)*stackqty;
+        var lossPercentage = (loss/initialprice)*100;
+
+       showOutput(`Hey!😔 you got the loss is ${loss} and the percentage is ${lossPercentage}%`)
+    }
+
+    else if(currentprice > initialprice){
+        var profit = (currentprice - initialprice)*stackqty;
+        var profitPercentage = (profit/currentprice)*100;
+
+       showOutput(`Yay!🤩 you got the profit is ${profit} and the percentage is ${profitPercentage}%`)
+    }
+
+    else{
+        showOutput("No pain no gain and No gain no pain"); 
+    }
+
+}
+
+function showOutput(msg){
+    
+    outputShown.innerText = msg;
+}
